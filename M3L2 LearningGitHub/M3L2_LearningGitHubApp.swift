573f61b76525f1c25aaa2297d267e775bb9dec11
Tommy Pirone <tpirone@rocketmail.com>
2021-07-13 15:51:01 -0400

    Initial Commit

---
+++ M3L2 LearningGitHub/M3L2_LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2_LearningGitHubApp.swift
+//  M3L2 LearningGitHub
+//
+//  Created by Pirone DJs on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct M3L2_LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
